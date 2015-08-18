8dcecce6125d13ace8ce70c40fa3432c74c0adbc
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-08-18 10:26:35 -0500

    Strings and arrays

--- day1.5/Program.cs
+++ day1.5/Program.cs
@@ -10,6 +10,38 @@ namespace day1._5
     {
         static void Main(string[] args)
         {
+            string one = "cat";
+            string two = "dog";
+            string three = "bird";
+            string four = "fish";
+
+            string someStr = "The quick brown fox jumped over the lazy dog";
+
+            Console.WriteLine(someStr.Remove(9) + "...");
+            Console.WriteLine(someStr);
+
+            //someStr.Substring(<startIdx>, ?<length>);
+            //someStr = someStr.Insert(8, "new string");
+
+            //string.IsNullOrEmpty(someStr.Trim());
+            //string.IsNullOrWhiteSpace(someStr);
+            //if (!!!!!!string.IsNullOrEmpty(someStr)) {
+            //}
+
+            //someStr.Remove(someStr.IndexOf("lazy"), 4);
+            //someStr = someStr.Replace("lazy", "");
+
+            //Console.WriteLine(string.Format("{1}{3}{2}", one, two, three, four));
+            //Console.WriteLine(one + " " + two + " " + three + " " + four);
+
+            //var colors = new int[3]
+            //{
+            //    1, 3, 2
+            //};
+            //Array.Sort(colors);
+            //Console.WriteLine(string.Join(", ", colors));
+
+            Console.ReadLine();
         }
     }
 }
